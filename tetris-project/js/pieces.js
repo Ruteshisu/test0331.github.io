24f00fc6d13d69ef2362fb4962c5460e83b8ca3b
Ruteshisu <nananijiroom@gmail.com>
2025-03-31 16:56:28 +0900

    Create pieces.js

---
+++ tetris-project/js/pieces.js
@@ -0,0 +1,55 @@
+// pieces.js
+
+// ブロックを生成する関数（形に応じて行列を返す）
+export function createPiece(type) {
+  switch(type) {
+    case 'T':
+      return [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0],
+      ];
+    case 'O':
+      return [
+        [2, 2],
+        [2, 2],
+      ];
+    case 'L':
+      return [
+        [0, 3, 0],
+        [0, 3, 0],
+        [0, 3, 3],
+      ];
+    case 'J':
+      return [
+        [0, 4, 0],
+        [0, 4, 0],
+        [4, 4, 0],
+      ];
+    case 'I':
+      return [
+        [0, 5, 0, 0],
+        [0, 5, 0, 0],
+        [0, 5, 0, 0],
+        [0, 5, 0, 0],
+      ];
+    case 'S':
+      return [
+        [0, 6, 6],
+        [6, 6, 0],
+        [0, 0, 0],
+      ];
+    case 'Z':
+      return [
+        [7, 7, 0],
+        [0, 7, 7],
+        [0, 0, 0],
+      ];
+  }
+}
+
+// 利用可能なブロックの種類をランダムに返す
+export function randomPieceType() {
+  const pieces = 'TJLOSZI';
+  return pieces[Math.floor(Math.random() * pieces.length)];
+}
